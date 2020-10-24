7e7ff6e24fde07855482c549a38649d6218a5678
narender bisht <narender007bisht@gmail.com>
2020-10-24 11:39:55 -0700

    Updates

---
+++ precache-manifest.63c7e97e8a2d2b30aad1eba2b726eb29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e9c14230dc9c0eb15c357d0022a33c",
+    "url": "/addusers-live/index.html"
+  },
+  {
+    "revision": "7b220027c1930c7866a9",
+    "url": "/addusers-live/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "a4ec47624aedcc61a46b",
+    "url": "/addusers-live/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "7b220027c1930c7866a9",
+    "url": "/addusers-live/static/js/2.8d1c9d83.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/addusers-live/static/js/2.8d1c9d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ec47624aedcc61a46b",
+    "url": "/addusers-live/static/js/main.f1cdf671.chunk.js"
+  },
+  {
+    "revision": "ca018d72bbcfdc5cc724",
+    "url": "/addusers-live/static/js/runtime-main.6776647b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crud-application/precache-manifest.c551176d317ae205ffacf73f1461585f.js"
+  "/addusers-live/precache-manifest.63c7e97e8a2d2b30aad1eba2b726eb29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crud-application/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/addusers-live/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
