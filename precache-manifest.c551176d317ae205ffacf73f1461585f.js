1f99d8defc90b98342165affd01cdb93332635b8
narender bisht <narender007bisht@gmail.com>
2020-10-23 23:54:52 -0700

    Updates

---
+++ precache-manifest.c551176d317ae205ffacf73f1461585f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a463debb14c17586b0633e18e0376581",
+    "url": "/crud-application/index.html"
+  },
+  {
+    "revision": "61ce19c8c6615c2fd641",
+    "url": "/crud-application/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "c8df0ab38bc0bccea9b3",
+    "url": "/crud-application/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "61ce19c8c6615c2fd641",
+    "url": "/crud-application/static/js/2.372b05ee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/crud-application/static/js/2.372b05ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8df0ab38bc0bccea9b3",
+    "url": "/crud-application/static/js/main.05d71478.chunk.js"
+  },
+  {
+    "revision": "2787f926c08c5dc0b586",
+    "url": "/crud-application/static/js/runtime-main.5c03ebd0.js"
+  }
+]);
